95afc31096d12041dc13fe581eab40af1c06cd8e
Семён Пупков <semenpupckov@yandex.ru>
2023-11-08 13:54:44 +0300

    project soon

--- temp.py
+++ temp.py
@@ -1,4 +1,37 @@
-def we_crash_all(name: str) -> str:    
-    return 'Привет, ' + name + ', мы всё сломали!'
+# character_creation_module/main.py
 
-print(we_crash_all(100)) 
+from random import randint
+
+# Новый импорт.
+# Из модуля start_game_banner, который расположен в папке graphic_arts,
+# импортируем функцию run_screensaver().
+from graphic_arts.start_game_banner import run_screensaver
+
+def attack(char_name: str, char_class: str) -> str:
+    ...
+
+def defence(char_name: str, char_class: str) -> str:
+    ...
+
+def special(char_name: str, char_class: str) -> str:
+    ...
+
+def start_training(char_name: str, char_class: str) -> str:
+    ...
+
+def choice_char_class() -> str:
+    ...
+
+def main() -> None:
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class))
+
+main()
